e8131862eab191273b7d2cf5b0a92927fb98b60b
Madi Vallarino <85126577+madivallarino@users.noreply.github.com>
2023-02-15 17:05:14 -0500

    first attempt

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -17,7 +17,7 @@ function Skill({ directionLeft, skill }: Props) {
         }}
         transition={{ duration: 1}}
         whileInView={{ opacity: 1, x:0 }} src={urlFor(skill?.image).url()} 
-        className="rounded-full border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"/>
+        className="rounded-full border-gray-500 object-cover w-10 h-10 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"/>
         <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-24 md:h-24 xl:w-32 xl:h-32 rounded-full z-0">
             <div className="flex items-center justify-center h-full">
                 <p className="text-3xl font-bold text-black opacity-100">{skill?.progress}%</p>
